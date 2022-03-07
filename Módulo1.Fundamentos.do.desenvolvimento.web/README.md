06d120060e4a04cd1e35a33e0e0d3b1e7b731f33
Jessica Milene <jmillene12@gmail.com>
2022-03-07 15:24:37 -0300

    fixação de apredizagem JS

--- Módulo1.Fundamentos.do.desenvolvimento.web/README.md
+++
@@ -1,3 +0,0 @@
-Local onde ficarão armazenadas de acordo com os blocos 
-referentes ao primeiro Módulo os exercicios que serão 
-realizados ao longo do curso
---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/myName.js
@@ -0,0 +1,20 @@
+// 1- Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+// 2 - Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+// 3 - Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+// 4 -Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+// 5 -Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+// 6 -Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+
+
+const myName = "jessica";
+const birthCity = "Piloes";
+let birthYear = "1992";
+
+birthYear = "2030";
+birthCity = "Candiba";
+
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
