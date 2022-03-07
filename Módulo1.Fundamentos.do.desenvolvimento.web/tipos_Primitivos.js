f359c2d58cdeab94dda02d31c3bceb9f53b37cf7
Jessica Milene <jmillene12@gmail.com>
2022-03-07 18:20:47 -0300

    calculando area e perimetro

--- Módulo1.Fundamentos.do.desenvolvimento.web/tipos_Primitivos.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/tipos_Primitivos.js
@@ -3,12 +3,18 @@ let isEnrolled = true;
 const patientInfo = {
   firstName: 'Ana',
   lastName: 'Santos',
+
 };
+const base = 5;
+let height = 8;
+const area = (base*height)
+const perimeter = ((base**2)+(height**2));
 const patientEmail = 'ana@email.com';
 
 console.log(typeof patientAge);
 console.log(typeof patientId);
-
+console.log(area);
+console.log(perimeter)
 
 
 // O que aconteceria se tentássemos checar qual o tipo da variável patientAge ? Experimente executar o comando console.log(typeof patientAge) e veja o que acontece! Ué...mas não declaramos essa variável, não é mesmo? É exatamente por esse motivo que o seu tipo é undefined , como você pôde observar no retorno do console.log(typeof patientAge) . Experimente também trocar o valor de patientId = 50 para patientId = '50' . Execute novamente um console.log() para imprimir o tipo dessa variável após a modificação. Você verá que o retorno agora é uma string pois colocamos o número 50 dentro das aspas. O JavaScript interpreta como string tudo o que estiver entre aspas.
