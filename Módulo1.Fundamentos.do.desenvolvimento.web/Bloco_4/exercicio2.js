1b88f8e1a229acb08e5f5e1b8e7ab1192850e427
Jessica Milene <jmillene12@gmail.com>
2022-03-08 11:43:02 -0300

    exercicio de comparaçao

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/exercicio2.js
@@ -0,0 +1,16 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 10;
+const b = 200;
+
+if (a > b){
+
+console.log("o valor de A é maior :", a)
+}
+
+else if (b > a){
+
+    console.log("o valor de B é maior:", b)
+}
+
+
