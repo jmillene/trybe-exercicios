96f7c4691e7ec29ce115fd058eb287ba76f9647e
Jessica Milene <jmillene12@gmail.com>
2022-03-08 21:53:43 -0300

    exercicio 4

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/exercicio4.js
@@ -0,0 +1,19 @@
+// Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const entrada = 1;
+
+if (entrada >= 1) {
+
+    console.log("positivo");
+
+}
+
+else if (entrada < 1){
+
+    console.log("negativo");
+}
+
+else {
+
+    console.log("zero");
+}
