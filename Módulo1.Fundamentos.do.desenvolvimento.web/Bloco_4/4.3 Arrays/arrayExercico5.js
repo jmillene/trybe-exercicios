57938dfff9403222d8603808092c1bcd2ca09806
Jessica Milene <jmillene12@gmail.com>
2022-03-11 09:37:43 -0300

    para fixar exercicio de objeto

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/4.3 Arrays/arrayExercico5.js
@@ -0,0 +1,7 @@
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let max = Math.max(...numbers)
+
+// console.log("O maior numero do meu array é  : " , max);
