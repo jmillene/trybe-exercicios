752782f618a14c6afcab27d2403732143f85d142
Jessica Milene <jmillene12@gmail.com>
2022-03-18 15:02:47 -0300

    exercicio de fixação fundamentos de JavaScript

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_4/Bloco_4_dia1/paraFixa1.js
@@ -0,0 +1,27 @@
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId );
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof patientAge)
+
+
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+// 1 - Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+
+const base = 5;
+const height =  8; 
+// 2 -Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+const area = base*height;
+console.log(area);
+// 3 -Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = 2*(base+height);
+
+console.log(perimeter);
