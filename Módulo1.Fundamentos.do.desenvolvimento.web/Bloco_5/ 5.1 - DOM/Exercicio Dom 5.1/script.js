c9daf9757875719c35512046a68a67797e754fc8
Jessica Milene <jmillene12@gmail.com>
2022-03-21 19:06:30 -0300

    adm de tempo finalizado

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_5/ 5.1 - DOM/Exercicio Dom 5.1/script.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_5/ 5.1 - DOM/Exercicio Dom 5.1/script.js
@@ -1,13 +1,18 @@
-// Aqui eu guardei os dados do meu Id na variavel cores para poder manipula-localStorage;
-let cores = document.getElementById("header-container");
-        cores.style.backgroundColor = 'blue';
+// mudei a cor do titulo das tarefas
+const mudarCorTitle = document.getElementById("header-container");{
+        mudarCorTitle.style.background  = 'blue'
+// _____________________________________________________________________
+const mudarCorBloco = document.querySelectorAll(".emergency-tasks");
+mudarCorBloco[0].style.background = 'red';
 
-// o "emergency-tasks" é uma class em uma section, então selecionei a classe,
-// devo informar a posição e o que desejo fazer na nela.
+const corTag = document.getElementsByTagName('h3');
+corTag[0].style.background = 'yellow';
+corTag[1].style.background = 'blue';
+}
 
+const mudarCorSegundoBloco = document.querySelectorAll(".no-emergency-tasks")
+mudarCorSegundoBloco[0].style.background = 'blue'
 
-let emergency =  document.getElementsByClassName("emergency-tasks")[0];
-emergency.style.backgroundColor = 'red';
-
-let emergencytasks = document.querySelectorAll('"emergency-tasks")')[1];
-emergency.style.backgroundColor = 'blue';
+const mudarCorTag = document.getElementsByTagName('h3');
+mudarCorTag[2].style.background = 'black';
+mudarCorTag[3].style.background = 'red';
--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_5/ 5.1 - DOM/Parte II - Seletores de elementos/script.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_5/ 5.1 - DOM/Parte II - Seletores de elementos/script.js
@@ -1,14 +1,15 @@
-const paragraph = document.getElementById("paragraph");{
+const paragraph = document.getElementById("paragraph");
 
-      paragraph.style.color = "orange"
+paragraph.style.color = "orange";
 
-      document.getElementById("page-title").innerText = "O Conde de monte Cristo"
-      document.getElementById("second-paragraph").innerText = "A volta dos que não foram"
-      document.getElementById("subtitle").innerText = "I love uuuuuuuu HTML"
-}
-      const paragrafo = document.getElementsByClassName('filmes');
-      paragrafo[0].style.color = 'yellow'
-      paragrafo[0].style.fontSize = '18px'
+document.getElementById("page-title").innerText = "O Conde de monte Cristo";
+document.getElementById("second-paragraph").innerText =
+  "A volta dos que não foram";
+document.getElementById("subtitle").innerText = "I love uuuuuuuu HTML";
+// Aqui usei o querySelectorAll para mudar as cores do meu titulo
+const paragrafo = document.querySelectorAll(".filmes");
+paragrafo[0].style.color = "yellow";
+paragrafo[0].style.fontSize = "18px";
 
-      const subtitle = document.getElementsByTagName('h4')[0]
-      subtitle.style.color = 'blue';
+const subtitle = document.querySelectorAll(".filmes");
+subtitle[1].style.color = "blue";
