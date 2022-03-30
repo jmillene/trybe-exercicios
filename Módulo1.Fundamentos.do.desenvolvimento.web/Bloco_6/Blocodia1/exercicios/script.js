0daf917fdfb57f38da6a4e45dfae960fae5439d7
jessica milene <jmillene12@gmail.com>
2022-03-30 19:27:20 -0300

    graças a Deus os botos funcionaram

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_6/Blocodia1/exercicios/script.js
@@ -0,0 +1,26 @@
+function buttonSubmit (event) {
+    event.preventDefault()
+
+}
+const btn = document.querySelector('#buttonSubmit');
+btn.addEventListener('click', buttonSubmit, false)
+
+//ref https://developer.mozilla.org/
+const input = document.querySelectorAll('input') //quero limpar meus inputs.
+const texto = document.querySelector('textarea')
+const botao = document.querySelector('#buttonApagar')
+botao.addEventListener('click', limpa)
+
+function limpa(){
+    //primeiro eu quero que o botão limpe tudo que está contido no meu input
+    //para isso posso fazer um for para que eu não digite elemento por elemento
+    for (let index =0; index < input.length; index+=1){
+        //agora ele deve passar de input por input e ir limpando
+        const limpando = input[index]
+        limpando.value = "" //meus inputs estão recebendo o valor vazio
+        limpando.checked = false //isso porque as caixinhas de checkount ficam preenchidas e tenho que tirar o check delas
+    }
+    texto.
+    value = ''
+}
+limpa();
