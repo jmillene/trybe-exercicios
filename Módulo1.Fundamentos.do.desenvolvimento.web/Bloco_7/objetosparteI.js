c7397421a09eb5c972977ea16ddea277bb718a4a
jessica milene <jmillene12@gmail.com>
2022-04-04 15:44:13 -0300

    add new  propriedades

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/objetosparteI.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/objetosparteI.js
@@ -36,3 +36,23 @@ const customer1 = {
   };
   customer2['lastName'] = 'Silva';
   console.log(customer2);
+
+//   No exemplo acima, ao invés de reescrever 
+//   o objeto inteiro, apenas adicionamos a 
+//   nova propriedade. Para isso, basta seguir a sintaxe abaixo:
+// objeto.nomeDaPropriedade .
+// objeto[variavelQueContemONomeDaPropriedade] .
+
+const customer = {
+    firstName: 'Roberto',
+    age: 22,
+    job: 'Teacher',
+  };
+  
+  let newKey = 'lastName';
+  const lastName = 'Ferreira';
+  customer[newKey] = lastName;
+  newKey = 'fullName';
+  const fullName = `${customer.firstName} ${customer.lastName}`;
+  customer[newKey] = fullName;
+  console.log(customer);
