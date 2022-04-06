ecb794a3f40af00dc0bf198aa752f97e489bba1b
Jessica Milene <jmillene12@gmail.com>
2022-04-06 16:42:54 -0300

    exercicio 2

---
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/exercicioParte1/exercicio2.js
@@ -0,0 +1,22 @@
+// Copie o código abaixo e faça uma função que
+// retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada
+// console.log(<seu código>oddsAndEvens<seu código>);
+// retorne "Os números 2,3,4,7,10,13 se encontram ordenados
+// de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando
+// o método array.sort() . Spoiler: É possível realizar uma
+// função que ordene qualquer array de números.
+// Copie o código abaixo.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const ordenarNumeros = _ =>
+  `Os números ${oddsAndEvens.sort((a,b) => a-b)} estão ordenados em ordem crescente!`;
+
+console.log(ordenarNumeros(oddsAndEvens));
+
+// Ref sort https://www.horadecodar.com.br/2020/03/18/javascript-como-ordernar-um-array/
+// será necessário alterar essa linha 😉
+
