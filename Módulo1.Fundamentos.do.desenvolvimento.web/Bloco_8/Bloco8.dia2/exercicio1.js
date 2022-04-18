91a5ea6a8a5bac15186190029da6e538f544ef70
Jessica Milene <jmillene12@gmail.com>
2022-04-18 19:34:19 -0300

    exercico 2

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_8/Bloco8.dia2/exercicio1.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_8/Bloco8.dia2/exercicio1.js
@@ -62,14 +62,14 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
-const expectedResult = [
-  "As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin",
-  "O Senhor dos Anéis - Fantasia - J. R. R. Tolkien",
-  "Fundação - Ficção Científica - Isaac Asimov",
-  "Duna - Ficção Científica - Frank Herbert",
-  "A Coisa - Terror - Stephen King",
-  "O Chamado de Cthulhu - Terror - H. P. Lovecraft",
-];
+// const expectedResult = [
+//   "As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin",
+//   "O Senhor dos Anéis - Fantasia - J. R. R. Tolkien",
+//   "Fundação - Ficção Científica - Isaac Asimov",
+//   "Duna - Ficção Científica - Frank Herbert",
+//   "A Coisa - Terror - Stephen King",
+//   "O Chamado de Cthulhu - Terror - H. P. Lovecraft",
+// ];
 
 function formatedBookNames() {
   // escreva seu código aqui
