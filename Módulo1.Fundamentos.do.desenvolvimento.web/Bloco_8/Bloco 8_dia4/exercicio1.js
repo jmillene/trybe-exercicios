9a2d0492d2a811dcca8ac2a41c05f6a76bb0511b
Jessica Milene <jmillene12@gmail.com>
2022-04-19 11:20:39 -0300

    exercicio 2

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_8/Bloco 8_dia4/exercicio1.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_8/Bloco 8_dia4/exercicio1.js
@@ -6,4 +6,5 @@ function flatten() {
   // escreva seu código aqui
   return arrays.reduce((acumulador, item) => acumulador.concat(item), []);
 }
+console.log(flatten(arrays));
 // ref https://pt.stackoverflow.com/questions/318038/como-transformar-um-array-de-arrays-em-um-%C3%BAnico-array-em-javascript
