85da2031685c9e3ef4548bb3ab1dc5479093610c
Jessica Milene <jmillene12@gmail.com>
2022-08-24 22:00:39 -0300

    exercicio 6.3

--- Módulo3.Desenvolvimento.Back.End/exercicio 22.1 Parte 2/io-local/readFileAsync.js
+++ Módulo3.Desenvolvimento.Back.End/exercicio 22.1 Parte 2/io-local/readFileAsync.js
@@ -1,3 +1,6 @@
+// exercicio 6.1
+//Crie uma função que leia todos os dados do arquivo e imprima cada personagem no 
+//formato id - Nome. Por exemplo: 1 - Homer Simpson.
 //const fs = require('fs').promises;
 // Crie uma função que leia todos os dados do arquivo e imprima cada 
 // personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
@@ -13,44 +16,38 @@
 // }
 // main();
 
-// Crie uma função que receba o id de uma personagem 
+// exercicio 6.2 Crie uma função que receba o id de uma personagem 
 // como parâmetro e retorne uma Promise que é resolvida 
 // com os dados da personagem que possui o id informado. Caso não haja uma personagem com o 
 // id informado, rejeite a Promise com o motivo "id não encontrado".
-const fs = require('fs').promises;
-
-async function verificarId (id){
-const promise = new Promise((resolve,reject) =>{
-  //const data =  await fs.readFile('./simpsons.json','utf8');
-  //const transformarJson = JSON.parse(data);
-  //const resultado = Object.keys(data).find((elemento)=> elemento.id === id);
-  console.log(data);
-if(!resultado){
-  reject(new Error('ID NÃO ENCONTRADO'))
-}
-  const resol = Object.keys(data).find((elemento)=> elemento.id === id) ;
- resolve(resol);
-});
-return promise;
-}
-const personagem = async(id) => {
-  const data =  await fs.readFile('./simpsons.json','utf8');
-  //console.log(data);
-  const transformarJson = JSON.parse(data);
-  const date = transformarJson.find((elemento)=> elemento.id === id);
-  if(date === undefined){
-  console.log('ID NÂO ENCONTRADO');
-  }
-  else {
-  console.log(date);
-}
-}
-personagem(1);
-// verificarId()
-// .then(result => console.log(`sucesso: ${result}`))
-// .catch(err => console.log(`erro: ${err.message}`));
-
+// const fs = require('fs').promises;
+// const personagem = async(id) => {
+//   const data =  await fs.readFile('./simpsons.json','utf8');
+//   //console.log(data);
+//   const transformarJson = JSON.parse(data);
+//   const date = transformarJson.find((elemento)=> elemento.id === id);
+//   if(date === undefined){
+//   console.log('ID NÂO ENCONTRADO');
+//   }
+//   else {
+//   console.log(date);
+// }
+// }
+// personagem(1);
 //pra ler é o parse
 //para escrever é o stringfy
+const fs = require('fs').promises;
+async function remover () {
+try {
+  const data = await fs.readFile('./simpsons.json', 'utf8');
+  const transformarJson = JSON.parse(data);
+  const date = transformarJson.filter((elemento)=>elemento.id !="1" && elemento.id != "10")
 
-//test
+  if(date.length === 8){
+    console.log("Arquivo removido com sucesso");
+  }
+} catch (error) {
+  console.log(`Esse arquivo não pode ser removido:${error.message}`);
+}
+}
+remover()
