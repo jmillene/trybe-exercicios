a89bdd3bcd5cc85aad6b1885f6500d352139b202
jessica <jmillene12@gmail.com>
2022-08-25 17:23:16 -0300

    exercicio metodo get

---
+++ Módulo3.Desenvolvimento.Back.End/exercicio 22.2/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/activities',(request,response)=> response.status(200).json({activities}))
+
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+module.exports = app;
---
+++ Módulo3.Desenvolvimento.Back.End/exercicio 22.2/src/server.js
@@ -0,0 +1,3 @@
+const port = 3009;
+const app = require('./app.js');
+app.listen(3009, ()=> console.log(`on na porta ${port}`));
