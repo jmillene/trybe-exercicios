76c686ef389a87dbc5c0059860c3f82fe000266a
Jessica Milene <jmillene12@gmail.com>
2022-11-04 19:09:27 -0300

    consultas mongo Db

--- Módulo3.Desenvolvimento.Back.End/exercicioPOOSessao9.2/dist/Teacher.js
+++ Módulo3.Desenvolvimento.Back.End/exercicioPOOSessao9.2/dist/Teacher.js
@@ -1,5 +1,4 @@
 "use strict";
-// ./Teacher.ts
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,19 +14,15 @@ class Teacher extends Person_1.default {
         this._admissionDate = new Date();
         this.registration = this.generateRegistration();
     }
+    generateRegistration() {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+        return `PRF${randomStr}`;
+    }
     get subject() {
         return this._subject;
     }
     set subject(value) {
-        this._subject = value;
-    }
-    get registration() {
-        return this._registration;
-    }
-    set registration(value) {
-        if (value.length < 16)
-            throw new Error('O registro deve possuir no mínimo 16 caracteres.');
-        this._registration = value;
+        this.subject = value;
     }
     get salary() {
         return this._salary;
@@ -41,13 +36,15 @@ class Teacher extends Person_1.default {
         return this._admissionDate;
     }
     set admissionDate(value) {
-        if (value.getTime() > new Date().getTime())
-            throw new Error('A data de admissão não pode ser uma data no futuro.');
         this._admissionDate = value;
     }
-    generateRegistration() {
-        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
-        return `PRF${randomStr}`;
+    get registration() {
+        return this._registration;
+    }
+    set registration(value) {
+        if (value.length < 16)
+            throw new Error('O registro deve possuir no mínimo 16 caracteres.');
+        this._registration = value;
     }
 }
 exports.default = Teacher;
